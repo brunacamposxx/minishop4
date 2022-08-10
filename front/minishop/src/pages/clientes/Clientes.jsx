d9ec34162d60126d80dda6587e3c60cfe465612f
Gabriela Silva Abreu <gabriela.abreu@iteris.com.br>
2022-08-10 14:16:04 -0300

    FEAT: adiciona card e lista de clientes

--- front/minishop/src/pages/clientes/Clientes.jsx
+++ front/minishop/src/pages/clientes/Clientes.jsx
@@ -1,13 +1,75 @@
 import React from 'react';
+import CardCliente from '../../components/cardCliente/CardCliente';
+import styles from './Clientes.module.css';
 
 function Clientes() {
+  const lista = [
+    {
+      id: 0,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 1,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 2,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 3,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 4,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 5,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 6,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 7,
+      nome: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      email: 'maria.anders@gmail.com',
+    },
+  ];
+
   return (
-    <div>
-      <p>cliente</p>
-      <p>cliente</p>
-      <p>cliente</p>
-      <p>cliente</p>
-      <p>cliente</p>
+    <div className={styles.pagina}>
+      <div className={styles.cabecalho}>
+        <h2>Clientes</h2>
+        <div className={styles.linha}></div>
+      </div>
+      <div className={styles.listacards}>
+        {lista.map((cliente) => (
+          <CardCliente
+            key={cliente.id}
+            nome={cliente.nome}
+            telefone={cliente.telefone}
+            email={cliente.email}
+          />
+        ))}
+      </div>
     </div>
   );
 }
