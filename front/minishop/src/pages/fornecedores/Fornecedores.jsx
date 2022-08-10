46b2469db3049dfc1dd6370439e7bb433e72cf47
Gabriela Silva Abreu <gabriela.abreu@iteris.com.br>
2022-08-10 14:31:33 -0300

    FEAT: adiciona card e lista de fornecedores

--- front/minishop/src/pages/fornecedores/Fornecedores.jsx
+++ front/minishop/src/pages/fornecedores/Fornecedores.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import CardFornecedor from '../../components/cardFornecedor/CardFornecedor';
+import styles from './Fornecedores.module.css';
 
 function Fornecedores() {
+  const lista = [
+    {
+      id: 0,
+      nome: 'Exotic Liquids',
+      contato: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      local: 'Uberlândia - MG',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 1,
+      nome: 'Exotic Liquids',
+      contato: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      local: 'Uberlândia - MG',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 2,
+      nome: 'Exotic Liquids',
+      contato: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      local: 'Uberlândia - MG',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 3,
+      nome: 'Exotic Liquids',
+      contato: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      local: 'Uberlândia - MG',
+      email: 'maria.anders@gmail.com',
+    },
+    {
+      id: 4,
+      nome: 'Exotic Liquids',
+      contato: 'Maria Anders',
+      telefone: '(11) 3003 4852',
+      local: 'Uberlândia - MG',
+      email: 'maria.anders@gmail.com',
+    },
+  ];
+
   return (
-    <div>
-      <p>fornecedor</p>
+    <div className={styles.pagina}>
+      <div className={styles.cabecalho}>
+        <h2>Fornecedores</h2>
+        <div className={styles.linha}></div>
+      </div>
+      <div className={styles.listacards}>
+        {lista.map((fornecedor) => (
+          <CardFornecedor
+            key={fornecedor.id}
+            nome={fornecedor.nome}
+            contato={fornecedor.contato}
+            telefone={fornecedor.telefone}
+            local={fornecedor.local}
+            email={fornecedor.email}
+          />
+        ))}
+      </div>
     </div>
   );
 }
