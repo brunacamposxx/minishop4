50a828d21d2b01e00b4637058f0a6b3a646d4423
vitor costa <vitor.costa@iteris.com.br>
2022-08-11 10:32:59 -0300

    FEAT:(orderItem+CustomerOrderResponse)

--- back/minishop/src/main/java/br/com/iteris/universidade/minishop/domain/dto/CustomerOrderResponse.java
+++ back/minishop/src/main/java/br/com/iteris/universidade/minishop/domain/dto/CustomerOrderResponse.java
@@ -1,24 +1,27 @@
 package br.com.iteris.universidade.minishop.domain.dto;
 
-import br.com.iteris.universidade.minishop.domain.entity.Customer;
 import br.com.iteris.universidade.minishop.domain.entity.CustomerOrder;
 import br.com.iteris.universidade.minishop.domain.entity.OrderItem;
 import lombok.Data;
-import org.springframework.stereotype.Service;
-
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class CustomerOrderResponse {
     private int Id;
     private Date OrderDate;
     private double TotalAmount;
-    private Customer customer;
+    private String FirstName;
+    private String LastName;
+    private List<OrderItem> Orders;
 
     public CustomerOrderResponse(CustomerOrder customerOrder) {
         Id = customerOrder.getCustomerId();
         OrderDate = customerOrder.getOrderDate();
         TotalAmount = customerOrder.getTotalAmount();
-        customer = customerOrder.getCustomer();
+
+        FirstName = customerOrder.getCustomer().getFirstName();
+        LastName = customerOrder.getCustomer().getLastName();
+        Orders = customerOrder.getOrderItems();
     }
 }
--- back/minishop/src/main/java/br/com/iteris/universidade/minishop/domain/dto/OrderItemIdResponse.java
+++ back/minishop/src/main/java/br/com/iteris/universidade/minishop/domain/dto/OrderItemIdResponse.java
@@ -4,16 +4,19 @@ import br.com.iteris.universidade.minishop.domain.entity.OrderItem;
 import lombok.Data;
 
 @Data
-public class OrderItemResponse {
+public class OrderItemIdResponse {
     private long Id;
     private Long ProductId;
+
+    private String ProductName;
     private double UnitPrice;
     private Integer Quantity;
 
-    public OrderItemResponse(OrderItem orderItem) {
+    public OrderItemIdResponse(OrderItem orderItem) {
         Id = orderItem.getId();
         ProductId = orderItem.getProductId();
         UnitPrice = orderItem.getUnitPrice();
         Quantity = orderItem.getQuantity();
+        ProductName = orderItem.getProduct().getProductName();
     }
 }
