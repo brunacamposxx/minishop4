b1d17ba4b85e483ca5075515616c987a0d454dc5
Bruna Campos Ferreira - BFI <bruna.ferreira@iteris.com.br>
2022-09-01 14:39:17 -0300

    feat: devops mandou

---
+++ back/minishop/src/main/java/br/com/iteris/universidade/minishop/component/MinishopS3Client.java
@@ -0,0 +1,51 @@
+package br.com.iteris.universidade.minishop.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class MinishopS3Client {
+
+    /**
+     * Informações de configuração do spring
+     * @see <a href="https://www.baeldung.com/spring-profiles">Spring Profiles</a>
+     */
+    private final Environment environment;
+
+    /**
+     * Recupera nome do bucket das configurações
+     * Caso não exista retorna a string "minishop-imagens"
+     */
+    @Value("${aws.bucket.name:minishop-imagens}")
+    private String bucketName;
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    /**
+     * Recupera conexão com AWS S3 de acordo com ambiente
+     * @see <a href="https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3.html">Working with Amazon S3</a>
+     * @see <a href="https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/s3/S3Client.html">Documentação</a>
+     * @return S3Client cliente de conexão
+     */
+    public S3Client getS3Client() {
+        // Em ambiente de desenvolvimento apontamos para o localstack
+        if (environment.acceptsProfiles(Profiles.of("default"))) {
+            return S3Client.builder()
+                    .endpointOverride(URI.create("http://localhost:4566"))
+                    .build();
+        }
+
+        // Em outros ambientes usamos a configuracao padrao
+        return S3Client.create();
+    }
+}
